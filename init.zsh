67dc718f9504418fa28cf02ac2def1054f2b6cbe
Zachary Elliott <zach@nyu.edu>
2013-06-26 18:17:59 -0400

    Adding custom config 'hooks'

--- init.zsh
+++ init.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 
 
+safe_source "$ZSH_CUSTOM/pre_init.zsh"
+
+
 fpath=( $ZSH_ROOT/function $ZSH_ROOT/completion $fpath )
 
 
@@ -14,5 +17,7 @@ for comp in $(\ls $ZSH_ROOT/completion); autoload "$comp"
 autoload -U compinit
 compinit -i
 
-
 source "$ZSH_ROOT/theme/master.zsh-theme"
+
+
+safe_source "$ZSH_CUSTOM/post_init.zsh"
--- install.zsh
+++ install.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-source ./zshenv 
+source ./zshenv
 
 ln -s "$ZSH_ROOT/zshenv" "$HOME/.zshenv"
 ln -s "$ZSH_ROOT/zshrc" "$HOME/.zshrc"
@@ -9,3 +9,4 @@ mkdir -p "$ZSH_ROOT/data"
 mkdir -p "$ZSH_ROOT/cache"
 mkdir -p "$ZSH_ROOT/function"
 mkdir -p "$ZSH_ROOT/completion"
+mkdir -p "$ZSH_ROOT/custom"
