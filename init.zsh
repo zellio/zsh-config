4cf4d921e7f43b0e95125de086a3a14ac3f5622b
Zachary Elliott <zach@nyu.edu>
2013-10-23 14:19:24 -0400

    changing completion dir name

--- init.zsh
+++ init.zsh
@@ -4,14 +4,14 @@
 safe_source "$ZSH_CUSTOM/pre_init.zsh"
 
 
-fpath=( $ZSH_ROOT/functions $ZSH_ROOT/completion $fpath )
+fpath=( $ZSH_ROOT/functions $ZSH_ROOT/completions $fpath )
 
 
 for conf ($ZSH_ROOT/zsh-config/*.zsh); source "$conf"
 
 for fn in $(\ls $ZSH_ROOT/functions); autoload "$fn"
 
-for comp in $(\ls $ZSH_ROOT/completion); autoload "$comp"
+for comp in $(\ls $ZSH_ROOT/completions); autoload "$comp"
 
 for conf ($ZSH_ROOT/app-config/*.zsh); source "$conf"
 
