2457e6ff904abfa14fa76bbaee6dda7ddac06df8
Zachary Elliott <zach@nyu.edu>
2013-10-23 14:22:49 -0400

    cleaning up install

--- install.zsh
+++ install.zsh
@@ -7,6 +7,6 @@ ln -s "$ZSH_ROOT/zshrc" "$HOME/.zshrc"
 
 mkdir -p "$ZSH_ROOT/data"
 mkdir -p "$ZSH_ROOT/cache"
-mkdir -p "$ZSH_ROOT/function"
-mkdir -p "$ZSH_ROOT/completion"
+mkdir -p "$ZSH_ROOT/functions"
+mkdir -p "$ZSH_ROOT/completions"
 mkdir -p "$ZSH_ROOT/custom"
