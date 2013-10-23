be865fead753b0299caad3ff2e0a24d4b43f231f
Zachary Elliott <zach@nyu.edu>
2013-10-23 15:46:46 -0400

    Updating init file and adding compeltions

--- init.zsh
+++ init.zsh
@@ -2,15 +2,15 @@
 
 safe_source "$ZSH_CUSTOM/pre_init.zsh"
 
-fpath=( $ZSH_ROOT/functions $ZSH_ROOT/completions $fpath )
+fpath=( $ZSH_ROOT/functions/***/(-/) $ZSH_ROOT/completions/***/(-/) $fpath )
 
-for conf ($ZSH_ROOT/config/zsh/*.zsh); source "$conf"
+for config ($ZSH_ROOT/config/zsh/*\.zsh); source $config
 
-for fn in $(\ls $ZSH_ROOT/functions); autoload "$fn"
+for fn ($ZSH_ROOT/functions/***/*(.)); autoload $fn:t
 
-for comp in $(\ls $ZSH_ROOT/completions); autoload "$comp"
+for comp ($ZSH_ROOT/completions/***/*(.)); autoload $comp:t
 
-for conf ($ZSH_ROOT/config/*.zsh); source "$conf"
+for conf ($ZSH_ROOT/config/*\.zsh); source $conf
 
 autoload -U compinit
 compinit -i
