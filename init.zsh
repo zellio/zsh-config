c4e2fbc10346a0ecdaebfd902ac12354539f662e
Zachary Elliott <zach@nyu.edu>
2013-10-23 14:21:54 -0400

    config restructure

--- init.zsh
+++ init.zsh
@@ -7,13 +7,13 @@ safe_source "$ZSH_CUSTOM/pre_init.zsh"
 fpath=( $ZSH_ROOT/functions $ZSH_ROOT/completions $fpath )
 
 
-for conf ($ZSH_ROOT/zsh-config/*.zsh); source "$conf"
+for conf ($ZSH_ROOT/config/zsh/*.zsh); source "$conf"
 
 for fn in $(\ls $ZSH_ROOT/functions); autoload "$fn"
 
 for comp in $(\ls $ZSH_ROOT/completions); autoload "$comp"
 
-for conf ($ZSH_ROOT/app-config/*.zsh); source "$conf"
+for conf ($ZSH_ROOT/config/*.zsh); source "$conf"
 
 autoload -U compinit
 compinit -i
