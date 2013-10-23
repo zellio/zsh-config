aef3cf1d8636887401cc1b04385c8f4385f7aa8f
Zachary Elliott <zach@nyu.edu>
2013-10-23 14:24:51 -0400

    Cleanin up init.zsh

--- init.zsh
+++ init.zsh
@@ -1,11 +1,8 @@
 #!/usr/bin/env zsh
 
-
 safe_source "$ZSH_CUSTOM/pre_init.zsh"
 
-
 fpath=( $ZSH_ROOT/functions $ZSH_ROOT/completions $fpath )
-
 
 for conf ($ZSH_ROOT/config/zsh/*.zsh); source "$conf"
 
@@ -18,7 +15,6 @@ for conf ($ZSH_ROOT/config/*.zsh); source "$conf"
 autoload -U compinit
 compinit -i
 
-source "$ZSH_ROOT/theme/master.zsh-theme"
-
+source "$ZSH_ROOT/theme/theme"
 
 safe_source "$ZSH_CUSTOM/post_init.zsh"
