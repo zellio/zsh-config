ab7f8d71385ef439b182f284ad375b825ef078b1
Zachary Elliott <contact@zell.io>
2016-03-11 17:17:00 -0500

    Flesh out zshrc init file

--- init.zsh
+++ init.zsh
@@ -1,1 +1,21 @@
 #!/usr/bin/env zsh
+
+fpath=(
+	"${ZSH_HOME}/functions"/***/(-/)
+	"${ZSH_HOME}/completions"/***/(-/)
+	$fpath
+)
+
+for config ( "${ZSH_HOME}/config"/**/*.zsh(N)
+			 "${ZSH_HOME}/custom"/**/*.zsh(N) ); do
+    source "$config:A"
+done
+
+for func ( "${ZSH_HOME}/functions"/***/*(.N)
+		   "${ZSH_HOME}/completions"/***/*(.N) ); do
+	autoload "$func:t"
+done
+
+autoload -U compinit
+
+compinit -i -d "${ZSH_HOME}/data/zcompdump"
