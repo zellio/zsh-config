7f90e0e920b654bca23d0f56eda5b0872d541a73
Zachary Elliott <contact@zell.io>
2016-03-12 22:39:04 -0500

    Add fn opts

---
+++ config/zsh/functions.zsh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+setopt c_bases
+setopt c_precedences
+setopt debug_before_cmd
+setopt NO_err_exit
+# setopt NO_err_return
+setopt eval_lineno
+# setopt exec
+setopt function_argzero
+setopt NO_local_loops
+setopt NO_local_options
+setopt NO_local_patterns
+setopt no_local_traps
+setopt NO_multi_func_def
+setopt multios
+setopt octal_zeroes
+setopt NO_pipe_fail
+setopt NO_source_trace
+setopt NO_typeset_silent
+setopt NO_verbose
+setopt NO_xtrace
