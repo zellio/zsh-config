51b7719bf5e085be7f74faf73f6ac802596d008b
Zachary Elliott <contact@zell.io>
2016-03-12 22:13:34 -0500

    Io option configs

---
+++ config/zsh/io.zsh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+setopt aliases
+setopt clobber
+setopt NO_correct
+setopt NO_correct_all
+setopt NO_dvorak
+setopt NO_flow_control
+setopt NO_ignore_eof
+setopt interactive_comments
+setopt hash_cmds
+setopt hash_dirs
+setopt NO_hash_executables_only
+setopt NO_mail_warning
+setopt NO_path_dirs
+setopt NO_path_script
+setopt NO_print_eight_bit
+setopt print_exit_value
+setopt rc_quotes
+setopt NO_rm_star_silent
+setopt rm_star_wait
+setopt short_loops
+setopt NO_sun_keyboard_hack
