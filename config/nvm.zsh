cb6f1aeed1e3628c90fbf8d1950a4210f2d1f4f9
Zachary Elliott <contact@zell.io>
2021-02-06 19:48:12 -0500

    Sets NVM to be lazy-loaded

--- config/nvm.zsh
+++ config/nvm.zsh
@@ -1,9 +1,15 @@
 #!/usr/bin/env zsh
 
-NVM_DIR="${HOME}/.nvm"
+NVM_DIR="${NVM_DIR:-${HOME}/.nvm}"
 
 [ ! -d "$NVM_DIR" ] && return 0
 
 export NVM_DIR
 
-source "${NVM_DIR}/nvm.sh"
+# NVM Takes forever to load so let's lazy load it when it actually gets
+# used. Probably want to make this smarter in the future.
+function nvm
+{
+    source "${NVM_DIR}/nvm.sh"
+    nvm "$@"
+}
